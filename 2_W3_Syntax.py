cb074f62f553cb4a8c4bac33dd03cd600ed93480
A2Misbehave <hello@a2misbehave.com>
2025-02-22 17:22:35 +1300

    Python Syntax

---
+++ 2_W3_Syntax.py
@@ -0,0 +1,29 @@
+"""
+Author: 	A2Misbehave
+Resource: 	W3 Schools - Python
+URL:		https://www.w3schools.com/python/python_syntax.asp
+"""
+
+# Identation
+
+if 5 > 2:
+	print("Five is greater than two!")
+
+"""
+Syntax Error
+
+if 5 > 2:
+print("Five is greater than two!") # Causes syntax error due to no indentation after a ':' on the previous line.
+"""
+
+"""
+There was an example about being able to choose my own level of identation and how I must then use that same level there after, but I got into programming during the time where 4 spaces = 1 tab and that also seems to be the default for Python instead of the newly adopted 2 spaces being 1 tab, so instead of exploring that and confusing myself, I will stick to 4 spaces as a tab so as not to confuse myself and get ideas about customising a 'tab' too much.
+"""
+
+# Variables
+
+x = 5
+y = "Hello, World!"
+
+# Comments - Yes, this is weird, as I'm already using comments quite a bit, but I needed to properly document my learnings' with Python and so here we are.
+print("Hello, World! This was preceded by a comment.")
