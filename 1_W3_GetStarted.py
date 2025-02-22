0606665224923721e2ef33a6e004126c8458855a
A2Misbehave <hello@a2misbehave.com>
2025-02-22 17:13:07 +1300

    Python Get Started

---
+++ 1_W3_GetStarted.py
@@ -0,0 +1,14 @@
+"""
+Author: 	A2Misbehave
+Resource: 	W3 Schools
+URL:		https://www.w3schools.com/python/python_getstarted.asp
+"""
+
+# Good Ole' Hello World
+
+print("Hello, World!")
+
+# Python Version
+
+import sys
+print(sys.version)
