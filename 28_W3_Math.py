accf9ae322d515897ec1d3782b110cb6d9c59324
A2Misbehave <hello@a2misbehave.com>
2025-03-01 20:06:37 +1300

    Python Math

---
+++ 28_W3_Math.py
@@ -0,0 +1,47 @@
+"""
+Author: 	A2Misbehave
+Resource: 	W3 Schools - Python
+URL:		https://www.w3schools.com/python/python_math.asp
+"""
+
+# Built-In Math Functions
+
+# Min / Max
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print("Built-In Math Function 'min':", x)
+print("Built-In Math Function 'max':", y)
+
+# Absolute Value
+
+x = abs(-7.25)
+print("Built-In Math Function 'abs':", x)
+
+# Power of Y
+
+x = pow(4, 3)
+print("Built-In Math Function 'pow':", x)
+
+# Math Module
+
+# Square Root
+
+import math
+
+x = math.sqrt(64)
+print("Math Module Square Root:", x)
+
+# Ceiling / Floor
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print("Math Module Ceiling:", x)
+print("Math Module Floor:", y)
+
+# PI
+
+x = math.pi
+print("Math Module PI:", x)
