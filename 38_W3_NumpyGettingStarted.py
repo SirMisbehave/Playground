e6b1b38b1d4935b4d4edf46cead45db79fd0bdb7
A2Misbehave <hello@a2misbehave.com>
2025-03-01 22:58:29 +1300

    Python NumPy Getting Started

---
+++ 38_W3_NumpyGettingStarted.py
@@ -0,0 +1,16 @@
+"""
+Author: 	A2Misbehave
+Resource: 	W3 Schools - Python
+URL:		https://www.w3schools.com/python/numpy/numpy_getting_started.asp
+"""
+
+# NumPy as np
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print("NumPy Array:", arr)
+
+# Checking NumPy Version
+
+print("NumPy Version:", np.__version__)
