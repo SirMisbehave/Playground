4b298fe1c4da68b7831e97ae138cc839c2110db4
A2Misbehave <hello@a2misbehave.com>
2025-03-01 22:05:55 +1300

    Python User Input

---
+++ 32_W3_UserInput.py
@@ -0,0 +1,10 @@
+"""
+Author: 	A2Misbehave
+Resource: 	W3 Schools - Python
+URL:		https://www.w3schools.com/python/python_user_input.asp
+"""
+
+# User Input
+
+username = input("Enter username:")
+print("Username is:" + username)
