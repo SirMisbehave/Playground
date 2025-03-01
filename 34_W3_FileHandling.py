ec925e25dec26df860872704effc6de6a95b2ad2
A2Misbehave <hello@a2misbehave.com>
2025-03-01 22:24:53 +1300

    Python File Handling

---
+++ 34_W3_FileHandling.py
@@ -0,0 +1,11 @@
+"""
+Author: 	A2Misbehave
+Resource: 	W3 Schools - Python
+URL:		https://www.w3schools.com/python/python_file_handling.asp
+"""
+
+# File Handling
+
+f = open("demofile.txt")
+
+f = open("demofile.txt", "rt") # 'r' for read, 't' for text are the default values.
